98cfa65d311bbeffd688e9196c822c0f8c58f533
lewis mcmillan <lewistmcmillan@gmail.com>
2022-10-17 15:24:32 +0100

    added r4 routines

--- src/time.f90
+++ src/time.f90
@@ -4,12 +4,23 @@ module time
 
     implicit none
 
+    interface get_time
+        module procedure get_timeR4
+        module procedure get_timeR8
+    end interface get_time
+
+    interface print_time
+        module procedure print_timeR4
+        module procedure print_timeR8
+    end interface print_time
+
+
     private
     public :: get_time, print_time
 
     contains
     
-        real function get_time()
+        real(kind=dp) function get_timeR8()
 
 #ifdef _OPENMP
             use omp_lib
@@ -18,10 +29,35 @@ module time
 #else
             call cpu_time(get_time)
 #endif
-        end function get_time
+        end function get_timeR8
+
+        real(kind=sp) function get_timeR4()
+
+#ifdef _OPENMP
+            use omp_lib
+
+            get_time = omp_get_wtime()
+#else
+            call cpu_time(get_time)
+#endif
+        end function get_timeR4
 
 
         subroutine print_time(time, id)
+
+            real(kind=sp), intent(IN) :: time
+            integer,       intent(IN) :: id
+
+            if(id == 0)then
+                if(time >= 60._sp)then
+                   print*, floor((time)/60._sp),"mins", mod(time, 60._sp)/100._sp,"s"
+                else
+                   print*, 'time taken ~',time,'s'
+                end if
+            end if
+        end subroutine print_timeR4
+
+        subroutine print_timeR8(time, id)
 
             real(kind=dp), intent(IN) :: time
             integer,       intent(IN) :: id
@@ -33,5 +69,6 @@ module time
                    print*, 'time taken ~',time,'s'
                 end if
             end if
-        end subroutine print_time
+        end subroutine print_timeR8
+
 end module time
